2c5c84f5de61f78e04bced71d2b61ea80cf09876
Md Imam Hussain <160214704+mihussain737@users.noreply.github.com>
2024-04-24 06:35:20 +0000

    added varaibles 2

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,9 @@
+"use strict"//treat as code as newer version
+
+let name="hitesh"
+let age=23
+let isLoggedIn=false
+
+console.log(typeof"hitesth")
+console.log(typeof 43);
+console.log(typeof null)
