a750af231fb3ee73d819a31e6880b699e4ca0436
Md Imam Hussain <160214704+mihussain737@users.noreply.github.com>
2024-04-24 06:59:46 +0000

    data conversion

---
+++ 01_basics/03_variables.js
@@ -0,0 +1,16 @@
+let score="33ab"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntNumber=Number(score)
+// console.log(typeof valueIntNumber)
+// console.log(valueIntNumber);
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
