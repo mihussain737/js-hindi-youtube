49c522af63cc07a3990b358da759bd1021007b89
Md Imam Hussain <160214704+mihussain737@users.noreply.github.com>
2024-05-21 04:36:05 +0000

    comparison

---
+++ 02_datatypes/05_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)// true
+// console.log(2<1);//false
+
+
+// console.log("2">1);//true convert string to num then compare
+// console.log("02">1);
+
+// console.log(null==0); //false
+// console.log(null>0);//false
+// console.log(null>=0);//true
+
+// console.log(undefined>0); //false
+// console.log(undefined==0); //false
+// console.log(undefined>=0); //false
+
+//== vs //===
+//convert then comparison in==
+//strict comparison in ===
+
+console.log("1"==1);//true
+console.log("1"===1);//false :-strict comparison of datatypes
