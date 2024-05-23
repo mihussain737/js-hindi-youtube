85e71ee43eb2496fc367d3efaf579133e76b3ec6
Md Imam Hussain <160214704+mihussain737@users.noreply.github.com>
2024-05-23 05:46:11 +0000

    array is added

---
+++ 03_Array/01_arrays.js
@@ -0,0 +1,27 @@
+let arr=[0,2,6,4,5];
+// console.log(arr)
+
+// arr.push(5)
+// console.log(arr);
+// arr.pop(arr)
+// console.log(arr);
+
+// arr.unshift(9)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// console.log(arr.includes(9))//false
+
+// console.log(arr.indexOf(5));
+
+// let newArr=arr.join()//convert string from array
+// console.log(newArr)
+
+const myn1=arr.slice(0,3)
+console.log(myn1);
+console.log(arr)
+
+const myn2=arr.splice(1,4)
+console.log(myn2)
+console.log(arr)
