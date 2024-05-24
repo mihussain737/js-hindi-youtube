30863d6a185bf823edf73768a44916e88ba65e94
Md Imam Hussain <160214704+mihussain737@users.noreply.github.com>
2024-05-24 06:31:19 +0000

    de-structured and api

---
+++ 04_objectsAndFunctions/03_objects.js
@@ -0,0 +1,15 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseIntstructor:"hitesh"
+}
+//console.log(course.courseIntstructor)
+const {courseIntstructor:instructor} =course;
+// console.log(courseIntstructor)
+console.log(instructor);
+
+//api
+// {
+//     "name": "hitesh",
+//     "email": "h@gmail.com"
+// }
