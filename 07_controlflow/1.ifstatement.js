540a0de3861d57121a5e87f0d24b23af98995513
Md Imam Hussain <160214704+mihussain737@users.noreply.github.com>
2024-05-26 06:02:23 +0000

    if else

---
+++ 07_controlflow/1.ifstatement.js
@@ -0,0 +1,17 @@
+//if else
+// const temperature=41;
+// if(temperature<50){
+//     console.log("temperature is less than 50")
+// }
+// else{
+//     console.log("temperature is greater than 50")
+// }
+
+
+// const score=200;
+// if(score>100) {
+//     let power="fly";
+//     console.log(`power is :${power}`)
+// }
+// console.log(`user power is ${power}`)//power cant be access because of limited access of let
+
